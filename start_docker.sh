2c644e25b195bcd8db9fd9bb4fe1c45f46383e5e
parzel <parzel@posteo.de>
2020-10-20 11:41:24 +0200

    fixed port issue

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build . -t fakedns
-docker run -it -P -v $(realpath config):/root/config --privileged --expose 53 -t fakedns
+docker run -it -P -v $(realpath config):/root/config --privileged -p 53:53/udp -t fakedns
